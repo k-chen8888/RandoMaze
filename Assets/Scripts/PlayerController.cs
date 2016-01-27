1870b05b89faf6c14f2b605e746a2ef796834e41
Kevin Chen <k.chen8880@gmail.com>
2016-01-26 19:04:00 -0500

    First commit, file>project settings>editor, force text asset serialization, visible metafiles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    // Information about the RigidBody
+    private Rigidbody rb;
+    public float speed = 3;
+
+    // Information about the Player object
+    private GameObject self;
+
+    // Use this for initialization
+    void Start()
+    {
+        self = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Well... are you?
+        AmIDead();
+
+        // Move the object
+        // Relative forces play better in the low-friction environment
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddRelativeForce(move * speed);
+    }
+
+    // For that moment when you're very, very dead
+    void AmIDead()
+    {
+        if (self.transform.position.y < -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallController : MonoBehaviour
+{
+    // Information about how this object moves
+    public float speed = 5;
+    public float moveDistance = 5;
+
+    // Information about the Player object
+    private GameObject player;
+
+    // Awaiting move orders
+    private bool moving;
+    private Vector3 startPosition,
+                    direction = Vector3.zero,
+                    targetLocation;
+    private float percentTravelled = 0.0f;
+    public float waitTime = 1.0f;
+    private float nextMoveTime;
+    
+
+    /* Variables for Utilities */
+
+    // Easing function
+    [Range(0, 2)]
+    public float easeFactor = 1;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        startPosition = transform.position;
+        targetLocation = transform.position;
+
+        // When to start moving
+        nextMoveTime = Time.time + waitTime;
+        moving = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Move randomly every once in a while
+        if (Time.time > nextMoveTime && !moving)
+        {
+            Vector3[] moveSettings = RandomMove();
+
+            // Save starting point
+            startPosition = transform.position;
+
+            // Set things in motion
+            direction = moveSettings[0];
+            targetLocation = moveSettings[1];
+            moving = true;
+        }
+
+        // Moving along...
+        Move(direction, targetLocation);
+    }
+
+
+    /* Utilities */
+
+    // An RNG to determine whether or not the wall moves at any given time
+    Vector3[] RandomMove()
+    {
+        Vector3 direction = Vector3.zero,
+                targetLocation = transform.position;
+
+        // The closer the player is to the wall, the more likely the wall is to move
+        if (Random.Range(0.0f, 1.0f) > (Vector3.Distance(this.transform.position, player.transform.position) - 2.5f) / 100.0f)
+        {
+            // Randomly decide which direction to go
+            // Do NOT move off the edge
+            switch (Random.Range(0, 4))
+            {
+                case 0: // Forwards
+                    if (this.transform.position.z < 47.0f)
+                    {
+                        direction = Vector3.forward;
+                        targetLocation = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + moveDistance);
+                    }
+
+                    break;
+
+                case 1: // Backwards
+                    if (this.transform.position.z > -47.0f)
+                    {
+                        direction = Vector3.back;
+                        targetLocation = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - moveDistance);
+                    }
+
+                    break;
+
+                case 2: // Left
+                    if (this.transform.position.x > -22.0f)
+                    {
+                        direction = Vector3.left;
+                        targetLocation = new Vector3(this.transform.position.x - moveDistance, this.transform.position.y, this.transform.position.z);
+                    }
+
+                    break;
+
+                case 3: // Right
+                    if (this.transform.position.x < 22.0f)
+                    {
+                        direction = Vector3.right;
+                        targetLocation = new Vector3(this.transform.position.x + moveDistance, this.transform.position.y, this.transform.position.z);
+                    }
+
+                    break;
+            }
+        }
+
+        return new Vector3[2] { direction, targetLocation };
+    }
+
+    // 
+    IEnumerator shooterTest()
+    {
+        while (true)
+        {
+            // Perform movements
+            
+
+            yield return new WaitForSeconds(1.0f);
+
+            // Try to generate a new place to move to
+            Vector3[] moveSettings = RandomMove();
+
+            // Save starting point
+            startPosition = transform.position;
+
+            // Set things in motion
+            direction = moveSettings[0];
+            targetLocation = moveSettings[1];
+            moving = true;
+        }
+    }
+
+    // Moves the object by translating a smooth movement
+    void Move(Vector3 direction, Vector3 targetLocation)
+    {
+        // Calculate easing between current and target locations
+        percentTravelled += Time.deltaTime * speed / moveDistance;
+        percentTravelled = Mathf.Clamp01(percentTravelled);
+        float easedPercent = Ease(percentTravelled);
+
+        // Calculate new position based on easing
+        Vector3 newPos = Vector3.Lerp(startPosition, targetLocation, easedPercent);
+
+        // Are we there yet?
+        if (percentTravelled >= 1.0f)
+        {
+            moving = false;
+            percentTravelled = 0.0f;
+
+            // Wait to start moving again
+            nextMoveTime = Time.time + waitTime;
+        }
+
+        transform.Translate(newPos - transform.position);
+    }
+
+    // Movement Easing equation: y = x^a / (x^a + (1-x)^a)
+    //
+    // Takes x values between 0 and 1 and maps them to y values also between 0 and 1
+    //  a = 1 -> straight line
+    //  This is a logistic function; as a increases, y increases faster for values of x near .5 and slower for values of x near 0 or 1
+    //
+    // For animation, 1 < a < 3 is pretty good
+    float Ease(float x)
+    {
+        float a = easeFactor + 1.0f;
+        return Mathf.Pow(x, a) / (Mathf.Pow(x, a) + Mathf.Pow(1 - x, a));
+    }
+}
